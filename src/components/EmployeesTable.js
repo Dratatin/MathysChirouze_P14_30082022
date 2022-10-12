6e931aa4ad56f5020f1b8787a881ba2632a6d708
Dratatin <matchirouze@orange.fr>
2022-10-12 10:02:23 +0200

    update display table with searchbar

--- src/components/EmployeesTable.js
+++ src/components/EmployeesTable.js
@@ -59,74 +59,80 @@ function EmployeesTable() {
 
     return (
         <div className="employee-table">
-            {table.length > 0 ?
+            {state.length > 0 ?
                 <Fragment>
                     <Searchbar handleChange={handleChange} input={input} />
-                    <TableContainer className="table-ctn">
-                        <Table className="table" aria-label="simple table">
-                            <TableHead className="table__tableHead">
-                                <TableRow className="table__tableHead__tableRow">
-                                    <TableCell className="table__tableHead__tableRow__tableCell">First Name</TableCell>
-                                    <TableCell className="table__tableHead__tableRow__tableCell">Last Name</TableCell>
-                                    <TableCell className="table__tableHead__tableRow__tableCell">Start Date</TableCell>
-                                    <TableCell className="table__tableHead__tableRow__tableCell">Departement</TableCell>
-                                    <TableCell className="table__tableHead__tableRow__tableCell">Date of Birth</TableCell>
-                                    <TableCell className="table__tableHead__tableRow__tableCell">Street</TableCell>
-                                    <TableCell className="table__tableHead__tableRow__tableCell">City</TableCell>
-                                    <TableCell className="table__tableHead__tableRow__tableCell">State</TableCell>
-                                    <TableCell className="table__tableHead__tableRow__tableCell">Zip Code</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody className="table__tableBody">
-                                {(rowsPerPage > 0
-                                    ? table.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                    : table
-                                ).map((employee) => (
-                                    <TableRow
-                                        key={employee.firstName}
-                                        className="table__tableBody__tableRow"
-                                    >
-                                        <TableCell className="table__tableBody__tableRow__tableCell">{employee.firstName}</TableCell>
-                                        <TableCell className="table__tableBody__tableRow__tableCell">{employee.lastName}test</TableCell>
-                                        <TableCell className="table__tableBody__tableRow__tableCell">{employee.startDate}</TableCell>
-                                        <TableCell className="table__tableBody__tableRow__tableCell">{employee.departement}</TableCell>
-                                        <TableCell className="table__tableBody__tableRow__tableCell">{employee.birthdate}</TableCell>
-                                        <TableCell className="table__tableBody__tableRow__tableCell">{employee.street}</TableCell>
-                                        <TableCell className="table__tableBody__tableRow__tableCell">{employee.city}</TableCell>
-                                        <TableCell className="table__tableBody__tableRow__tableCell">{employee.state}</TableCell>
-                                        <TableCell className="table__tableBody__tableRow__tableCell">{employee.zipCode}</TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                    {isBreakpoint ?
-                        <TablePagination
-                            rowsPerPageOptions={[{ label: 'All', value: -1 }]}
-                            component="div"
-                            count={table.length}
-                            rowsPerPage={rowsPerPage}
-                            page={page}
-                            onPageChange={handleChangePage}
-                            onRowsPerPageChange={handleChangeRowsPerPage}
-                            className="table-pagination"
-                        />
+                    {table.length > 0 ?
+                        <Fragment>
+                            <TableContainer className="table-ctn">
+                                <Table className="table" aria-label="simple table">
+                                    <TableHead className="table__tableHead">
+                                        <TableRow className="table__tableHead__tableRow">
+                                            <TableCell className="table__tableHead__tableRow__tableCell">First Name</TableCell>
+                                            <TableCell className="table__tableHead__tableRow__tableCell">Last Name</TableCell>
+                                            <TableCell className="table__tableHead__tableRow__tableCell">Start Date</TableCell>
+                                            <TableCell className="table__tableHead__tableRow__tableCell">Departement</TableCell>
+                                            <TableCell className="table__tableHead__tableRow__tableCell">Date of Birth</TableCell>
+                                            <TableCell className="table__tableHead__tableRow__tableCell">Street</TableCell>
+                                            <TableCell className="table__tableHead__tableRow__tableCell">City</TableCell>
+                                            <TableCell className="table__tableHead__tableRow__tableCell">State</TableCell>
+                                            <TableCell className="table__tableHead__tableRow__tableCell">Zip Code</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody className="table__tableBody">
+                                        {(rowsPerPage > 0
+                                            ? table.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                            : table
+                                        ).map((employee) => (
+                                            <TableRow
+                                                key={employee.firstName}
+                                                className="table__tableBody__tableRow"
+                                            >
+                                                <TableCell className="table__tableBody__tableRow__tableCell">{employee.firstName}</TableCell>
+                                                <TableCell className="table__tableBody__tableRow__tableCell">{employee.lastName}test</TableCell>
+                                                <TableCell className="table__tableBody__tableRow__tableCell">{employee.startDate}</TableCell>
+                                                <TableCell className="table__tableBody__tableRow__tableCell">{employee.departement}</TableCell>
+                                                <TableCell className="table__tableBody__tableRow__tableCell">{employee.birthdate}</TableCell>
+                                                <TableCell className="table__tableBody__tableRow__tableCell">{employee.street}</TableCell>
+                                                <TableCell className="table__tableBody__tableRow__tableCell">{employee.city}</TableCell>
+                                                <TableCell className="table__tableBody__tableRow__tableCell">{employee.state}</TableCell>
+                                                <TableCell className="table__tableBody__tableRow__tableCell">{employee.zipCode}</TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                            {isBreakpoint ?
+                                <TablePagination
+                                    rowsPerPageOptions={[{ label: 'All', value: -1 }]}
+                                    component="div"
+                                    count={table.length}
+                                    rowsPerPage={rowsPerPage}
+                                    page={page}
+                                    onPageChange={handleChangePage}
+                                    onRowsPerPageChange={handleChangeRowsPerPage}
+                                    className="table-pagination"
+                                />
+                                :
+                                <TablePagination
+                                    rowsPerPageOptions={[5, 10, 15, { label: 'All', value: -1 }]}
+                                    component="div"
+                                    count={table.length}
+                                    rowsPerPage={rowsPerPage}
+                                    page={page}
+                                    onPageChange={handleChangePage}
+                                    onRowsPerPageChange={handleChangeRowsPerPage}
+                                    className="table-pagination"
+                                    sx={{
+                                        ".MuiPaper-root": {
+                                            boxShadow: "none",
+                                        }
+                                    }}
+                                />
+                            }
+                        </Fragment>
                         :
-                        <TablePagination
-                            rowsPerPageOptions={[5, 10, 15, { label: 'All', value: -1 }]}
-                            component="div"
-                            count={table.length}
-                            rowsPerPage={rowsPerPage}
-                            page={page}
-                            onPageChange={handleChangePage}
-                            onRowsPerPageChange={handleChangeRowsPerPage}
-                            className="table-pagination"
-                            sx={{
-                                ".MuiPaper-root": {
-                                    boxShadow: "none",
-                                }
-                            }}
-                        />
+                        <p>No employees to display.</p>
                     }
                 </Fragment>
                 :
